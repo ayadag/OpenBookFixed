6070c559215886f71ba3103975c671aa083ebf9c
96ayad <96ayad@gmail.com>
2024-06-12 12:28:50 +0300

    index

--- pages/pool/create/index.tsx
+++ pages/pool/create/index.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 "use client";
 import Layout from '../../../components/layouts/layout';
 import { CreatePool } from '../../../components/pool/CreatePool';
@@ -10,22 +9,4 @@ const Pool = () => {
         </Layout>
     )
 }
-=======
-"use client";
-// import Layout from '../../../components/layouts/layout';
-// import { CreatePool } from '../../../components/pool/CreatePool2';
-
-import { CreatePool3 } from '../../../components/pool/CreatePool3';
-
-// import { CreatePool } from '../../../components/pool/CreatePool';
-
-const Pool = () => {
-    return(
-        // <Layout>
-            <CreatePool3/>
-            // <CreatePool/>
-        // </Layout>
-    )
-}
->>>>>>> b6f28704a847510d3c7c6418b1ea4627e7cf5ccb
 export default Pool;
