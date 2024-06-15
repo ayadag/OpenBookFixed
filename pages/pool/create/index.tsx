ae57a82cdd203620b7bdf06e730f70af7d5b2447
96ayad <96ayad@gmail.com>
2024-06-15 13:40:00 +0300

    //

--- pages/pool/create/index.tsx
+++ pages/pool/create/index.tsx
@@ -1,5 +1,4 @@
 "use client";
-// import CreatePool from '../../../components/createPool';
 import {
   SubmitHandler,
   useForm,
@@ -30,11 +29,10 @@ const Pool = () => {
 
     return(
         <Layout>
-            {/* <CreatePool/> */}
             <>
       <div className="space-y-4 mb-6">
         <div>
-          <h1 className="text-2xl text-slate-200">Create Market</h1>
+          <h1 className="text-2xl text-slate-200">Create Pool</h1>
           <p className="text-sm text-slate-400">
             You must set a custom RPC endpoint to create a market. There is no development fees therefore I am not 
             providing a base RPC endpoint. You can set a custom RPC endpoint by clicking the settings icon and pasting the URL there.
